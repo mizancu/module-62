16e79405c1efe72401d6a3ebbb90e9cdfd483303
mizancu <mizancu16@yahoo.com>
2022-05-15 00:41:46 +0600

    till-v-4

---
+++ src/pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p><small>Copyright @ /dynamic korte hobe /</small></p>
+        </footer>
+    );
+};
+
+export default Footer;
