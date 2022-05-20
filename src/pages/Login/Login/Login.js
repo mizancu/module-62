d1099646820a4a9cc633011909b8a20a19403abe
mizancu <mizancu16@yahoo.com>
2022-05-20 23:05:09 +0600

    module-62-1

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -3,6 +3,7 @@ import { Button, Form } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../Firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -36,16 +37,11 @@ const Login = () => {
             <h2 className='text-primary text-center mt-2'>Please login</h2>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter Email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Enter Password" required/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
@@ -55,6 +51,7 @@ const Login = () => {
                 </Button>
             </Form>
             <p>New to Genius Car? <Link to='/register' className='text-danger pe-auto text-decoration-none' onClick={navigateToRegister}>Please Register</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
---
+++ src/pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SocialLogin = () => {
+      return (
+            <div>
+                  <div className='d-flex  align-item-center'>
+                        <div style={{height:'1px'}} className='bg-primary w-50 mt-4'></div>
+                        <p className='mx-2 fs-4 '>or</p>
+                        <div style={{height:'1px'}} className='bg-primary w-50 mt-4'></div>
+                  </div>
+            </div>
+      );
+};
+
+export default SocialLogin;
